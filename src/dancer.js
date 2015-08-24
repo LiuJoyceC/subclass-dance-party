0bc4ba8babea81de0f9be23ff2b8bab937b9500e
Joyce Liu <liujoycec@gmail.com>
2015-08-24 15:54:12 -0700

    Refactored dancer.js and blinkyDancer.js to be pseudoclassical, and added keyword 'new' in init.js to create new instances of the dancers.

--- src/dancer.js
+++ src/dancer.js
@@ -1,20 +1,13 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
 
-  var dancer = {};
-
+  this.timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
 
+  this.step();
 
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left){
+  this.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
@@ -22,12 +15,17 @@ var makeDancer = function(top, left, timeBetweenSteps){
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition(top, left);
 
-  return dancer;
 };
+
+makeDancer.prototype.step = function(){
+    // the basic dancer doesn't do anything interesting at all on each step,
+    // it just schedules the next step
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  };
